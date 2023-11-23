62f9cfd7229d7d58e08cd20dc2ebba80d66a3c4a
Paulo Ricardo <pauloricardopaa@gmail.com>
2023-11-23 15:41:15 -0300

    Lualine plugin

---
+++ lua/plugins/core/lualine.lua
@@ -0,0 +1,38 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+    },
+    config = function ()
+        local lualine = require('lualine')
+
+        lualine.setup({
+            options = {
+                icons_enabled = true,
+                theme = 'tokyonight',
+                component_separators = ' ',
+                section_separators = { left = '', right = '' },
+                globalstatus = true,
+                ignore_focus = {'NvimTree_1'},
+            },
+            sections = {
+                lualine_a = { 'mode'},
+                lualine_b = { 'branch', 'diff', 'diagnostics', },
+                lualine_c = { {
+                    'buffers',
+                    hide_filename_extension = true,
+                    symbols = {
+                        modified = ' ●',      -- Text to show when the buffer is modified
+                        alternate_file = '󰁣 ', -- Text to show to identify the alternate file
+                        directory =  '',     -- Text to show when the buffer is a directory
+                    },
+                    max_length = vim.o.columns / 2,
+                } },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' },
+            }
+
+        })
+    end
+}
