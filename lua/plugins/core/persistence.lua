0803bcdd358ef696f26f75e577508b53cd303c5d
Paulo Ricardo <pauloricardopaa@gmail.com>
2023-11-25 15:35:00 -0300

    Persistence plugin. Allows you to restore sessions

---
+++ lua/plugins/core/persistence.lua
@@ -0,0 +1,18 @@
+return {
+    'folke/persistence.nvim',
+    event = 'BufReadPre', -- this will only start session saving when an actual file was opened
+    config = function()
+        local persistence = require('persistence')
+        local nmap = require('core.keymaps').nmap
+
+        persistence.setup({})
+
+        nmap('<leader>ql', function()
+            require('persistence').load({ last = true })
+        end, 'Restore last session')
+
+        nmap('<leader>qd', persistence.load, 'Restore the session for the current directory')
+        nmap('<leader>qx', persistence.stop, 'Session wont be saved on exit')
+
+    end
+}
