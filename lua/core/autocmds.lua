5f8dc32d39bd411f62cff1dd1f929675d8488225
Paulo Ricardo <pauloricardopaa@gmail.com>
2023-11-25 17:38:08 -0300

    Auto-pairs plugin. Formatting options for CSS files.

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,19 +2,20 @@ local M = {}
 
 local user_au = vim.api.nvim_create_augroup("user_au", { clear = true })
 
--- vim.api.nvim_create_autocmd({
---     "",
--- }, {
---     pattern = { "" },
---     callback = function()
---
---     end,
---     desc = "",
---     once = false,
---     group = { "user_au" },
--- })
-
 M.load = function()
+    vim.api.nvim_create_autocmd({
+        "BufEnter", 
+    }, {
+        pattern = { '*.css' },
+        callback = function()
+            vim.opt.formatoptions:remove('c')
+            vim.opt.formatoptions:remove('r')
+            vim.opt.formatoptions:remove('o')
+        end,
+        desc = "",
+        once = true,
+        group = user_au,
+    })
 end
 
 return M
---
+++ lua/plugins/core/auto-pairs.lua
@@ -0,0 +1,18 @@
+return {
+    'windwp/nvim-autopairs',
+    opts = {
+        map_c_h = true,
+        map_c_w = true,
+        fast_wrap = {
+            map = '<M-e>',
+            chars = { '{', '[', '(', '"', "'" },
+            pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+            offset = 0, -- Offset from pattern match
+            end_key = ".",
+            keys = 'qwertyuiopzxcvbnmasdfghjkl',
+            check_comma = true,
+            highlight = 'PmenuSel',
+            highlight_grey = 'LineNr',
+        },
+    },
+}
