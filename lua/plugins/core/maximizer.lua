cf52f8a0f7c120bcf96c9f538c0fe66c441b8e1a
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-01-28 14:18:30 -0300

    Vim-Maximizer plugin. Maximize windows in neovim

---
+++ lua/plugins/core/maximizer.lua
@@ -0,0 +1,8 @@
+return {
+    'szw/vim-maximizer',
+    config = function ()
+        local nmap = require('core.keymaps').nmap
+
+        nmap('<leader>m', '<cmd>MaximizerToggle<CR>', 'Maximize current window')
+    end,
+}
