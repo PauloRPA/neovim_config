93425ab304c3a02b5f703a5654920b589b218357
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-02-19 13:39:59 -0300

    LuaSnip plugin. Friendly-snippets plugin. vim-snippets plugin. nvim-cmp and LuaSnip configured with basic functionalities.

--- config/nvim/lua/plugins/core/nvim-cmp.lua
+++ config/nvim/lua/plugins/core/nvim-cmp.lua
@@ -4,11 +4,18 @@ return {
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
+        'L3MON4D3/LuaSnip',
     },
     config = function()
         local cmp = require('cmp')
+        local luasnip = require('luasnip')
 
         cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end
+            },
             mapping = cmp.mapping.preset.insert({
                 ['<C-d>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior, count = 4 }),
                 ['<C-u>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior, count = 4 }),
@@ -16,8 +23,6 @@ return {
                 ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior, count = 1 }),
                 ['<C-n>'] = cmp.mapping.scroll_docs(4),
                 ['<C-p>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-h>'] = cmp.mapping.close(),
-                ['<Esc>'] = cmp.mapping.abort(),
                 ['<Tab>'] = cmp.mapping.confirm( { behavior = cmp.ConfirmBehavior.Insert, select = true } ),
             }),
             window = {
@@ -25,6 +30,7 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             sources = cmp.config.sources({
+                { name = 'luasnip', keyword_length = 2 },
                 { name = 'buffer', keyword_length = 3 },
                 { name = 'path', keyword_length = 3 },
             })
