44149b6c04dc28519bc704d10d8ceb726f7853bf
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-03-07 12:04:17 -0300

    Nvim-Recorder plugin.

---
+++ config/nvim/lua/plugins/core/recorder.lua
@@ -0,0 +1,8 @@
+return {
+    'chrisgrieser/nvim-recorder',
+    dependencies = { 'rcarriga/nvim-notify', },
+    opts = {
+        slots = { 'a', 'p', 'x' },
+        lessNotifications = false,
+    },
+}
