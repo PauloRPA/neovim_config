86a0bd19c1679149978ce061fc430b5b2e14c8c9
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-03-07 11:54:30 -0300

    Notify's notifications can now be searched using Telescope, and there are some minor changes in Notify's configuration.

--- config/nvim/lua/plugins/core/notify.lua
+++ config/nvim/lua/plugins/core/notify.lua
@@ -1,6 +1,19 @@
 return {
     'rcarriga/nvim-notify',
+    dependencies = {
+        { 'nvim-telescope/telescope.nvim' },
+    },
     config = function()
         vim.notify = require('notify')
+
+        local nmap = require('core.keymaps').nmap
+        nmap('<leader>sn', '<cmd>Telescope notify<CR>', 'Search notifications')
+
+        require('notify').setup({
+            stages = 'fade',
+            render = 'compact',
+            timeout = 800,
+            max_width = 100,
+        })
     end,
 }
