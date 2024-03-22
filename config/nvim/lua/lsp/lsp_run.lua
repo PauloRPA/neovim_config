e8da73bacc36fd358d05a054ec4073af625c4514
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-03-22 10:50:04 -0300

    [Fix] Neodev now sets up before Lspconfig setup.

--- config/nvim/lua/lsp/lsp_run.lua
+++ config/nvim/lua/lsp/lsp_run.lua
@@ -1,11 +1,37 @@
 local M = {}
+local before_func = {}
+local after_func = {}
+
+--- Function to run before lspconfig
+---@param before_function function to run
+M.add_before = function(before_function)
+    if type(before_function) ~= 'function' then
+        vim.notify('Only functions may run before lspconfig.')
+        return
+    end
+    table.insert(before_func, before_function)
+end
+
+--- Function to run after lspconfig
+---@param after_function function to run
+M.add_after = function(after_function)
+    if type(after_function) ~= 'function' then
+        vim.notify('Only functions may run after lspconfig.')
+        return
+    end
+    table.insert(after_func, after_function)
+end
 
 M.before = function()
-
+    for _, func in ipairs(before_func) do
+        func()
+    end
 end
 
 M.after = function()
-
+    for _, func in ipairs(after_func) do
+        func()
+    end
 end
 
 return M
--- config/nvim/lua/plugins/lsp/neodev.lua
+++ config/nvim/lua/plugins/lsp/neodev.lua
@@ -1,16 +1,21 @@
 return {
     'folke/neodev.nvim',
     init = function()
-        require('neodev').setup({
-            library = {
-                enabled = true,  -- when not enabled, neodev will not change any settings to the LSP server
-                runtime = true,  -- runtime path
-                types = true,    -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
-                plugins = true,  -- installed opt or start plugins in packpath
-            },
-            setup_jsonls = true, -- configures jsonls to provide completion for project specific .luarc.json files
-            lspconfig = true,
-            pathStrict = true,
-        })
+        local function neodev_setup()
+            require('neodev').setup({
+                library = {
+                    enabled = true,  -- when not enabled, neodev will not change any settings to the LSP server
+                    runtime = true,  -- runtime path
+                    types = true,    -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
+                    plugins = true,  -- installed opt or start plugins in packpath
+                },
+                setup_jsonls = true, -- configures jsonls to provide completion for project specific .luarc.json files
+                lspconfig = true,
+                pathStrict = true,
+            })
+        end
+
+        local lsp_run = require('lsp.lsp_run')
+        lsp_run.add_before(neodev_setup)
     end
 }
