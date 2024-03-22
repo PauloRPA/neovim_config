2ba6776780b7d735c75d9467b7270b8e5a316d39
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-03-22 12:36:44 -0300

    Skel plugin.

---
+++ config/nvim/lua/plugins/core/skel.lua
@@ -0,0 +1,29 @@
+return {
+    'motosir/skel-nvim',
+    config = function()
+        local skel = require('skel-nvim')
+        local skeld = require('skel-nvim.defaults')
+
+        skel.setup({
+            skel_enabled = true,
+            apply_skel_for_empty_file = true,
+
+            substitutions = {
+                ['FILENAME'] = skeld.get_filename,
+                ['NAME'] = skeld.get_author,
+                ['DATE'] = skeld.get_date,
+                ['CPP_HDR_GUARD'] = skeld.get_cppheaderguard,
+                ['CPP_TEST_HDR_GUARD'] = skeld.get_testheaderguard,
+                ['CPP_HDR_INCLUDE'] = skeld.get_headerinclude,
+                ['CLASS_NAME'] = skeld.get_classname2,
+                ['NAMESPACE_OPEN'] = skeld.get_namespaceopen,
+                ['NAMESPACE_CLOSE'] = skeld.get_namespaceclose,
+            },
+
+            templates_dir = vim.fn.stdpath('config') .. '/assets/templates',
+            mappings = {
+                -- ['*.java'] = 'java.skel',
+            }
+        })
+    end
+}
