b5650ad2b9c2c307ce1659a6bf4b70f944e65281
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-05-05 09:07:52 -0300

    [Removed] Html-css plugin and its references due to a bug.

--- config/nvim/lua/plugins/core/nvim-cmp.lua
+++ config/nvim/lua/plugins/core/nvim-cmp.lua
@@ -48,9 +48,6 @@ return {
             },
             formatting = {
                 format = function(entry, vim_item)
-                    if entry.source.name == 'html-css' then
-                        vim_item.menu = entry.completion_item.menu
-                    end
                     if entry.source.name == 'nvim_lsp' then
                         local server_name = entry.source.source.client.config.name
                         if server_name == 'emmet_ls' then
@@ -72,21 +69,6 @@ return {
                 }
             },
             sources = cmp.config.sources({
-                {
-                    name = 'html-css',
-                    option = {
-                        enable_on = {
-                            'html'
-                        },                                           -- set the file types you want the plugin to work on
-                        file_extensions = { 'css', 'sass', 'less' }, -- set the local filetypes from which you want to derive classes
-                        style_sheets = {
-                            -- example of remote styles, only css no js for now
-                            'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-                            -- 'https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css',
-                        }
-                    },
-                    group_index = 1
-                },
                 { name = 'nvim_lsp_signature_help', group_index = 1 },
                 { name = 'nvim_lsp', group_index = 1 },
                 { name = 'luasnip', group_index = 1 },
