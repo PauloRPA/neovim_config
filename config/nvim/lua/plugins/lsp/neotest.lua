8c94a9a7702e7b042563aeaea90aa723ad72a7a6
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-08-23 10:08:43 -0300

    Neotest new keymaps.

--- config/nvim/lua/plugins/lsp/neotest.lua
+++ config/nvim/lua/plugins/lsp/neotest.lua
@@ -11,22 +11,27 @@ return {
         local neotest = require('neotest')
         local nmap = require('core.keymaps').nmap
 
-        nmap('<leader>ts', function()
+        nmap('<leader>ot', function()
             neotest.summary.toggle()
         end, 'Toggle neotest summary')
-        nmap('<leader>to', function()
+
+        nmap('<leader>oo', function()
             neotest.output.open()
         end, 'Open neotest output')
 
+        nmap('<leader>oO', function()
+            neotest.output_panel.toggle()
+        end, 'Open neotest output panel')
+
         neotest.setup({
+            output = {
+                enabled = true,
+                open_on_run = false,
+            },
             status = {
                 enabled = true,
                 signs = true,
-                virtual_text = false,
-            },
-            diagnostic = {
-                enabled = true,
-                severity = 1,
+                virtual_text = true,
             },
             adapters = {
                 require('neotest-java')({
