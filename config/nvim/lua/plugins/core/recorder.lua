6cf638b28ac2c67585af1f8835e5ab4607111270
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-08-24 11:52:32 -0300

    Nvim recorder configuration update.

--- config/nvim/lua/plugins/core/recorder.lua
+++ config/nvim/lua/plugins/core/recorder.lua
@@ -3,6 +3,28 @@ return {
     dependencies = { 'rcarriga/nvim-notify' },
     opts = {
         slots = { 'a', 'p', 'x' },
-        lessNotifications = false,
+        lessNotifications = true,
+        mapping = {
+            startStopRecording = 'q',
+            playMacro = 'Q',
+            switchSlot = '<C-q>',
+            editMacro = 'cq',
+            deleteAllMacros = 'dq',
+            yankMacro = 'yq',
+            -- ⚠️ this should be a string you don't use in insert mode during a macro
+            addBreakPoint = '^^',
+        },
+        performanceOpts = {
+            countThreshold = 100,
+            lazyredraw = true, -- enable lazyredraw (see `:h lazyredraw`)
+            noSystemClipboard = true, -- remove `+`/`*` from clipboard option
+            autocmdEventsIgnore = { -- temporarily ignore these autocmd events
+                'TextChangedI',
+                'TextChanged',
+                'InsertLeave',
+                'InsertEnter',
+                'InsertCharPre',
+            },
+        },
     },
 }
