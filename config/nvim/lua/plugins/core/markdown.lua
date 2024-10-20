6e9f4863bae9b3d656e7483987cd916100e46f7d
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-10-20 13:33:42 -0300

    'cmp_docs' are now treated as Markdown files when editing Java files

--- config/nvim/lua/plugins/core/markdown.lua
+++ config/nvim/lua/plugins/core/markdown.lua
@@ -6,11 +6,29 @@ return {
         local mkdn = vim.api.nvim_create_augroup('markdown-docs.view', { clear = true })
 
         vim.api.nvim_create_autocmd({ 'FileType' }, {
+            pattern = { 'java' },
+            callback = function()
+                vim.api.nvim_create_autocmd({ 'FileType' }, {
+                    pattern = { 'cmp_docs' },
+                    callback = function()
+                        vim.cmd.setl('ft=markdown')
+                    end,
+                    desc = 'Treat docs-view as md files',
+                    once = true,
+                    group = mkdn,
+                })
+            end,
+            desc = 'Treat cmp_docs as md files when opening a java file',
+            once = true,
+            group = mkdn,
+        })
+
+        vim.api.nvim_create_autocmd({ 'FileType' }, {
             pattern = { 'nvim-docs-view' },
             callback = function()
                 vim.cmd.setl('ft=markdown')
             end,
-            desc = 'Treat docs-view to md files',
+            desc = 'Treat docs-view as md files',
             once = true,
             group = mkdn,
         })
