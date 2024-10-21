b45b0572ef85709989d4caaee6abf881cd723927
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-10-21 09:06:55 -0300

    Focus plugin

---
+++ config/nvim/lua/plugins/core/focus.lua
@@ -0,0 +1,50 @@
+return {
+    'nvim-focus/focus.nvim',
+    version = '*',
+    config = function()
+        local focus = require('focus')
+        local ignore_filetypes = { 'dapui_watches', 'dap-repl', 'dapui_stacks', 'dapui_scopes' }
+        local focusGroup = vim.api.nvim_create_augroup('focus_group', { clear = true })
+
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = ignore_filetypes,
+            callback = function(_)
+                vim.b.focus_disable = true
+            end,
+            desc = 'Disable focus autoresize for dap-ui buffers',
+            group = focusGroup,
+        })
+
+        focus.setup({
+            enable = true, -- Enable module
+            commands = true, -- Create Focus commands
+            autoresize = {
+                enable = true, -- Enable or disable auto-resizing of splits
+                width = 0, -- Force width for the focused window
+                height = 0, -- Force height for the focused window
+                minwidth = 0, -- Force minimum width for the unfocused window
+                minheight = 0, -- Force minimum height for the unfocused window
+                height_quickfix = 10, -- Set the height of quickfix panel
+            },
+            split = {
+                bufnew = false, -- Create blank buffer for new split windows
+                tmux = false, -- Create tmux splits instead of neovim splits
+            },
+            ui = {
+                number = false, -- Display line numbers in the focussed window only
+                relativenumber = false, -- Display relative line numbers in the focussed window only
+                hybridnumber = false, -- Display hybrid line numbers in the focussed window only
+                absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
+
+                cursorline = true, -- Display a cursorline in the focussed window only
+                cursorcolumn = false, -- Display cursorcolumn in the focussed window only
+                colorcolumn = {
+                    enable = false, -- Display colorcolumn in the foccused window only
+                    list = '+1', -- Set the comma-saperated list for the colorcolumn
+                },
+                signcolumn = true, -- Display signcolumn in the focussed window only
+                winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
+            },
+        })
+    end,
+}
