98452d2aabe5307f1464f0ff395c0bd910c04e56
Paulo Ricardo <pauloricardopaa@gmail.com>
2025-01-09 12:33:01 -0300

    [Feat] Vhs tape skeleton adicionado

--- config/nvim/lua/plugins/core/skel.lua
+++ config/nvim/lua/plugins/core/skel.lua
@@ -4,11 +4,25 @@ return {
         local skel = require('skel-nvim')
         local skeld = require('skel-nvim.defaults')
 
+        local function simple_filename()
+            return vim.fn.fnamemodify(vim.fn.expand('%'), ':p:t:r')
+        end
+
+        local mappings = {
+            -- ['*.java'] = 'java.skel',
+        }
+        local skel_template_path = require('core.info').path_skel_templates
+        for _, current_skel in pairs(vim.fn.readdir(skel_template_path)) do
+            local filename = vim.fn.fnamemodify(vim.fn.expand(current_skel), ':p:t:r')
+            mappings['*.' .. filename] = current_skel
+        end
+
         skel.setup({
             skel_enabled = true,
             apply_skel_for_empty_file = true,
 
             substitutions = {
+                ['SIMPLE_FILENAME'] = simple_filename,
                 ['FILENAME'] = skeld.get_filename,
                 ['NAME'] = skeld.get_author,
                 ['DATE'] = skeld.get_date,
@@ -21,9 +35,7 @@ return {
             },
 
             templates_dir = require('core.info').path_skel_templates,
-            mappings = {
-                -- ['*.java'] = 'java.skel',
-            },
+            mappings = mappings,
         })
     end,
 }
