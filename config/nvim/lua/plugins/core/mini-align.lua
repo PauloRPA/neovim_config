0feedd0cd3d911ea7d03e2e300100b76ec3c1f7b
Paulo Ricardo <pauloricardopaa@gmail.com>
2025-01-10 16:16:10 -0300

    [Plugin] Mini.Align

---
+++ config/nvim/lua/plugins/core/mini-align.lua
@@ -0,0 +1,10 @@
+return {
+    'echasnovski/mini.align',
+    version = false,
+    opts = {
+        mappings = {
+            start = 'ga',
+            start_with_preview = 'gA',
+        },
+    },
+}
