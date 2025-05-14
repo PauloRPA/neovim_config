51efd94b05dbeb74fc4f5427e206de966dbd889a
Paulo Ricardo <pauloricardopaa@gmail.com>
2025-05-14 15:24:19 -0300

    [Config] Keymap para pesquisar o conteudo da area de transferencia adicionado

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -161,6 +161,7 @@ M.load = function()
     -- User functions
     nmap('<Space>', '<Nop>', 'Nop')
     nmap('<leader>0', '/\\-\\-\\-\\<CR>', 'Empty search')
+    nmap('<leader>?', '/<C-r>0<CR>', 'Empty search')
     imap('<A-p>', '<ESC>pa', 'Paste content and return to insert mode')
 
     nmap('<leader>dc', function()
