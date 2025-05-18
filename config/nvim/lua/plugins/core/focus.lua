0e4002e4c1ada17742b288e1561125c552d76572
Paulo Ricardo <pauloricardopaa@gmail.com>
2025-05-18 12:06:15 -0300

    [Plugin] Focus agora usa mini animate ao trocar o foco do painel

--- config/nvim/lua/plugins/core/focus.lua
+++ config/nvim/lua/plugins/core/focus.lua
@@ -2,6 +2,32 @@ return {
     'nvim-focus/focus.nvim',
     enabled = true,
     version = '*',
+    dependencies = {
+        {
+            'echasnovski/mini.animate',
+            version = false,
+            config = function()
+                require('mini.animate').setup({
+                    cursor = {
+                        enable = false,
+                    },
+                    scroll = {
+                        enable = false,
+                    },
+                    resize = {
+                        -- Whether to enable this animation
+                        enable = true,
+
+                        -- Timing of animation (how steps will progress in time)
+                        timing = function()
+                            return 0.1;
+                        end
+                    },
+
+                })
+            end
+        },
+    },
     config = function()
         local focus = require('focus')
         local focusGroup = vim.api.nvim_create_augroup('focus_group', { clear = true })
@@ -27,34 +53,34 @@ return {
         })
 
         focus.setup({
-            enable = true, -- Enable module
-            commands = true, -- Create Focus commands
+            enable = true,            -- Enable module
+            commands = true,          -- Create Focus commands
             autoresize = {
-                enable = true, -- Enable or disable auto-resizing of splits
-                width = 0, -- Force width for the focused window
-                height = 0, -- Force height for the focused window
-                minwidth = 0, -- Force minimum width for the unfocused window
-                minheight = 0, -- Force minimum height for the unfocused window
+                enable = true,        -- Enable or disable auto-resizing of splits
+                width = 0,            -- Force width for the focused window
+                height = 0,           -- Force height for the focused window
+                minwidth = 0,         -- Force minimum width for the unfocused window
+                minheight = 0,        -- Force minimum height for the unfocused window
                 height_quickfix = 10, -- Set the height of quickfix panel
             },
             split = {
                 bufnew = false, -- Create blank buffer for new split windows
-                tmux = false, -- Create tmux splits instead of neovim splits
+                tmux = false,   -- Create tmux splits instead of neovim splits
             },
             ui = {
-                number = false, -- Display line numbers in the focussed window only
-                relativenumber = false, -- Display relative line numbers in the focussed window only
-                hybridnumber = false, -- Display hybrid line numbers in the focussed window only
+                number = false,                    -- Display line numbers in the focussed window only
+                relativenumber = false,            -- Display relative line numbers in the focussed window only
+                hybridnumber = false,              -- Display hybrid line numbers in the focussed window only
                 absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
 
-                cursorline = true, -- Display a cursorline in the focussed window only
-                cursorcolumn = false, -- Display cursorcolumn in the focussed window only
+                cursorline = true,                 -- Display a cursorline in the focussed window only
+                cursorcolumn = false,              -- Display cursorcolumn in the focussed window only
                 colorcolumn = {
-                    enable = false, -- Display colorcolumn in the foccused window only
-                    list = '+1', -- Set the comma-saperated list for the colorcolumn
+                    enable = false,                -- Display colorcolumn in the foccused window only
+                    list = '+1',                   -- Set the comma-saperated list for the colorcolumn
                 },
-                signcolumn = true, -- Display signcolumn in the focussed window only
-                winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
+                signcolumn = true,                 -- Display signcolumn in the focussed window only
+                winhighlight = false,              -- Auto highlighting for focussed/unfocussed windows
             },
         })
     end,
